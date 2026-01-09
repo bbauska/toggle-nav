e5d3a2ecc6fa69500058e84641c6cff6c84dd613
Brian Bauska <brianbauska@gmail.com>
2026-01-09 08:42:59 -0800

    Create scripts-2.js

---
+++ js/scripts-2.js
@@ -0,0 +1,14 @@
+// ./js/script-2.js of toggle-nav
+function toggleDiv(){
+  if ($(window).width() < 768) {
+    $("nav.navbar-default").hide();
+  } else{
+    $("nav.navbar-default").show();
+  }
+}
+$(document).ready(function () {
+  toggleDiv();
+  $(window).resize(function(){
+    toggleDiv();
+  });
+});
